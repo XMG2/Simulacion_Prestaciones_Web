602a691e99c43cfb84d82e8bc40fb1a2f838fdf0
Oihana Elizagoyen <elizagoyen.131910@e.unavarra.es>
2024-01-21 21:19:11 +0100

    Realizado el primer bloque de cuestiones. Existen dudas con respecto a la cuestión 5 de dicho bloque. Se le preguntará a mendivil acerca de dicha cuestión.

--- utils_ecuaciones.py
+++ utils_ecuaciones.py
@@ -1,38 +1,24 @@
-def cal_tasas_llegada(tasa_op_global, v_a, v_b, f_a, f_b):
+import numpy as np
+
+def calcular_tasa_operaciones_por_estado(tasa_global, v_a, v_b, f_a, f_b):
+    """Calculo de λr
+    tasa_global = tasa media de llegada de operaciones global (λ)
+    v_a = porcentaje de visitas del usuario A a cada estado
+    v_b = porcentaje de visitas del usuario B a cada estado
+    f_a = frecuencia de aparición del usuario A
+    f_b = frecuencia de aparición del usuario B
+    """
     tasas_llegada = []
     for v_a_i, v_b_i in zip(v_a, v_b):
-        tasas_llegada.append(tasa_op_global * (v_a_i * f_a + v_b_i * f_b))
+        tasas_llegada.append(tasa_global * (v_a_i * f_a + v_b_i * f_b))
     return tasas_llegada
 
 
-def cal_tiempo_res_op_rec(demanda: int, tasas_llegada: list, ni: int):
-    """Calculo de Tir
-    Ni = nº de unidades del recurso i
-    demanda = D_ir
-    tasas_llegada = lambda_r
-    """
-    return demanda / (1 - (sum(tasas_llegada) / ni) * demanda)
-
-
-def cal_tiempo_res_op(
-    demandas_op: list[int], tasas_llegada: list[int], vector_ni: list[int]
-) -> int:
-    """Calculo de Tr
-    vector_ni = nº de unidades de los recursos
-    demandas_op = D_r
-    tasas_llegada = lambda_r
-    """
-    tiempos_res_op = []
-    for demanda, ni in zip(demandas_op, vector_ni):
-        tiempos_res_op.append(cal_tiempo_res_op_rec(demanda, tasas_llegada, ni))
-    return sum(tiempos_res_op)
-
-
-def cal_utilizacion_recurso(tasas_llegada, demandas_rec, ni):
-    """Calculo de Ui,
-    Ni = nº de unidades del recurso i,
-    demandas_rec = D_i,
-    tasas_llegada = lambda_r
+def calcular_utilizacion_recurso(tasas_llegada, demandas_rec, ni):
+    """Cálculo de Ui,
+    ni = nº de unidades del recurso i
+    demandas_rec = demanda del recurso i por las diferentes operaciones(D_i)
+    tasas_llegada = tasa de llegada de operaciones para cada estado r (vector de λr)
     """
     sumatorio = 0
     for demanda, tasa in zip(demandas_rec, tasas_llegada):
@@ -40,9 +26,40 @@ def cal_utilizacion_recurso(tasas_llegada, demandas_rec, ni):
     return sumatorio / ni
 
 
-def cal_tiempo_med_res_op(tasas_llegada, tasa_op_global, tiempos_res_op):
-    """Calculo de Tr"""
+def calcular_tiempo_residencia_operacion_por_recurso(demandas, tasas_llegada, vector_n, i, r):
+    """Cálculo de Tir
+    demandas = matriz de demandas (D)
+    tasas_llegada = tasa de llegada de operaciones para cada estado (vector de λr)
+    vector_n = vector que contiene el nº de unidades de cada recurso
+    i = índice del recurso
+    r = índice de la clase de operación
+    """
+    return demandas[i, r] / (
+        1 - calcular_utilizacion_recurso(tasas_llegada, demandas[:, i], vector_n[i])
+    )
+
+
+def calcular_tiempo_respuesta_por_operacion(demandas, tasas_llegada, vector_n):
+    """Cálculo del valor Tr para cada clase de operación (vector de Tr) 
+    demandas = matriz de demandas (D)
+    tasas_llegada = tasa de llegada de operaciones para cada estado (vector de λr)
+    vector_n = vector que contiene el nº de unidades de cada recurso
+    """
+    T = np.zeros(demandas.shape)
+    for i in range(len(demandas)):
+        for r in range(len(demandas[i])):
+            T[i, r] = calcular_tiempo_residencia_operacion_por_recurso(demandas, tasas_llegada, vector_n, i, r)
+    # sumar la matriz por filas
+    return np.sum(T, axis=1)
+
+
+def calcular_tiempo_medio_respuesta_operaciones(tasas_llegada, tasa_global, tiempos_res_op):
+    """Cálculo de T
+    tasas_llegada = tasa de llegada de operaciones para cada estado r (vector de λr)
+    tasa_global = tasa media de llegada de operaciones global (λ)
+    tiempos_res_op = tiempo de respuesta para cada operación r (vector de Tr)
+    """
     sumatorio = 0
     for tasa_llegada, tiempo_res_op in zip(tasas_llegada, tiempos_res_op):
         sumatorio += tasa_llegada * tiempo_res_op
-    return sumatorio / tasa_op_global
+    return sumatorio / tasa_global
